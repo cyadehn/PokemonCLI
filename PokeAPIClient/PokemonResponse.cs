459a6a47fbd7833fc71173470e1f0efe355f08a6
Chris Dehner <topherdehner@gmail.com>
2020-10-21 00:26:39 -0400

    Initial commit

---
+++ PokeAPIClient/PokemonResponse.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PokeAPIClient
+{
+    public class PokemonResponse
+    {
+        public int Count { get; set; } 
+        public string Next { get; set; } 
+        public object Previous { get; set; } 
+        //[JsonPropertyName("results")]
+        public List<Pokemon> results { get; set; } 
+
+        public class Pokemon    
+        {
+            public string name { get; set; } 
+            public string url { get; set; } 
+        }
+    }
+}
---
+++ PokeAPIClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace PokeAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await ProcessPokemon();
+        }
+
+        private static async Task ProcessPokemon()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add( new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "PokeAPI Pokemon Lister");
+            var streamTask = client.GetStreamAsync("https://pokeapi.co/api/v2/pokemon/");
+            PokemonResponse response = await JsonSerializer.DeserializeAsync<PokemonResponse>(await streamTask);
+            foreach (var pokemon in response.results)
+            {
+                Console.WriteLine(pokemon.name);
+            }
+        }
+    }
+}
